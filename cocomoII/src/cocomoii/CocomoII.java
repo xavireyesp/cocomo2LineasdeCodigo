0b30722240a5ef217569f07c787a730b3c4b8014
LORENA <LORENA@DESKTOP-10N2PJK>
2020-08-07 12:31:04 -0500

    agrege la clase ingresar

--- cocomoII/src/cocomoii/CocomoII.java
+++ cocomoII/src/cocomoii/CocomoII.java
@@ -14,7 +14,7 @@ public class CocomoII {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) {//
         // TODO code application logic here
         
         System.out.println("Ejemplo");
@@ -24,7 +24,5 @@ public class CocomoII {
         
         System.out.println("Esta si");
         
-        
-    }
-    
+        System.out.println("sii");    
 }
