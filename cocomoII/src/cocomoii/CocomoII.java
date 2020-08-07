852a045158326ac5624f9b7a99de4e0e2acbfe50
Eduardo Reyes <>
2020-08-07 00:09:15 -0500

    Primera subida

--- cocomoII/src/cocomoii/CocomoII.java
+++ cocomoII/src/cocomoii/CocomoII.java
@@ -16,6 +16,8 @@ public class CocomoII {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Ejemplo");
     }
     
 }
