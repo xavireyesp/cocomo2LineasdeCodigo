a7ca1bd8fc82570624344d0f9cb378b929c9d9e0
Eduardo Reyes <Asus@192.168.1.111>
2020-08-23 22:46:59 -0500

    Pruebas Unitarias

--- cocomoII/test/TestLeerArchivo.java
+++ cocomoII/test/TestLeerArchivo.java
@@ -4,41 +4,34 @@
  * and open the template in the editor.
  */
 
+import controlador.LeerArchivo;
+import java.io.BufferedReader;
+import java.io.File;
+import java.util.ArrayList;
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Rule;
+import org.junit.rules.TemporaryFolder;
 
 /**
  *
  * @author Asus
  */
 public class TestLeerArchivo {
-    
-    public TestLeerArchivo() {
-    }
-    
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    @Before
-    public void setUp() {
-    }
-    
-    @After
-    public void tearDown() {
+
+    @Test
+    public void test_scan() throws Exception {
+        LeerArchivo leer = new LeerArchivo(); // with args
+        int a =  leer.contarLineas(); 
+        
+        
+        
+
+// If you arrive here, it"s OK
     }
 
-    // TODO add test methods here.
-    // The methods must be annotated with annotation @Test. For example:
-    //
-    // @Test
-    // public void hello() {}
 }
